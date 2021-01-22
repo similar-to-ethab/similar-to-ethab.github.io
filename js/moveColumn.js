c7361d934620b1ad9a03fba311acb3b92ab3a30b
similar-to-ethab <61324293+similiar-to-ethab@users.noreply.github.com>
2021-01-22 12:34:32 -0500

    aligned footer correctly, need to fix footer body alignment

--- js/moveColumn.js
+++ js/moveColumn.js
@@ -1,19 +1,23 @@
 function topOffset() {
-
+  //var $el = $('#column_left')
+  var bottom_loc = $('#column_left').offset().top + document.getElementById("column_left").style.height;
+  console.log(bottom_loc);
   if ($('#column_left')[0].getBoundingClientRect().top < 0 && !$('#column_left').hasClass('fixed')) {
 
     $('#column_left').removeClass('absolute');
     $('#column_left').addClass('fixed');
-    console.log("more",$('#column_left').offset().top < 160,$('#column_left').offset().top);
+    //console.log("more",$('#column_left').offset().top < 160,$('#column_left').offset().top);
   }
-  else if ($('#column_left').offset().top <= 80 && !$('#MyElement').hasClass('absolute')){
+  else if ($('#column_left').offset().top <= 80 && !$('#column_left').hasClass('absolute')){
 
       $('#column_left').removeClass('fixed');
       $('#column_left').addClass('absolute');
-      console.log("less",$('#column_left')[0].getBoundingClientRect().top);
+      //console.log("less",$('#column_left')[0].getBoundingClientRect().top);
   }
+  //else if ($('#column_left').offset().top)
+
   else {
-    console.log("I broke it.");
+    console.log("I broke it or it is past.");
   }
 
       //console.log($('#column_left').offset().top, "offset");
