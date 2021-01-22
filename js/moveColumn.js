d1ff120b3cd3e4d962b11409baeb3f66a29d20f7
similar-to-ethab <61324293+similiar-to-ethab@users.noreply.github.com>
2021-01-21 22:30:58 -0500

    oops

--- js/moveColumn.js
+++ js/moveColumn.js
@@ -1,15 +1,16 @@
 function topOffset() {
-  if ($('#column_left')[0].getBoundingClientRect().top <= 0 && !$('#column_left').hasClass('fixed')) {
+  
+  if ($('#column_left')[0].getBoundingClientRect().top < 0 && !$('#column_left').hasClass('fixed')) {
 
     $('#column_left').removeClass('absolute');
     $('#column_left').addClass('fixed');
-    console.log("more",$('#column_left').offset().top <= 160);
+    console.log("more",$('#column_left').offset().top < 160,$('#column_left').offset().top);
   }
   else if ($('#column_left').offset().top <= 160 && !$('#MyElement').hasClass('absolute')){
 
       $('#column_left').removeClass('fixed');
       $('#column_left').addClass('absolute');
-      console.log("less");
+      console.log("less",$('#column_left')[0].getBoundingClientRect().top);
   }
   else {
     console.log("I broke it.");
