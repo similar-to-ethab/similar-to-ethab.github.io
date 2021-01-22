4176cca3b138c0102cb4e8787ed2c7cf4b7e8a86
similar-to-ethab <61324293+similiar-to-ethab@users.noreply.github.com>
2021-01-21 23:04:50 -0500

    FIXED IT!!!!!!!
    no more glitchyness

--- js/moveColumn.js
+++ js/moveColumn.js
@@ -1,12 +1,12 @@
 function topOffset() {
-  
+
   if ($('#column_left')[0].getBoundingClientRect().top < 0 && !$('#column_left').hasClass('fixed')) {
 
     $('#column_left').removeClass('absolute');
     $('#column_left').addClass('fixed');
     console.log("more",$('#column_left').offset().top < 160,$('#column_left').offset().top);
   }
-  else if ($('#column_left').offset().top <= 160 && !$('#MyElement').hasClass('absolute')){
+  else if ($('#column_left').offset().top <= 80 && !$('#MyElement').hasClass('absolute')){
 
       $('#column_left').removeClass('fixed');
       $('#column_left').addClass('absolute');
