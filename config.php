53bd3b67fce2923569eba87df05738e130c90132
Ethan Smith <61324293+similar-to-ethab@users.noreply.github.com>
2021-02-05 08:50:24 -0500

    will be moved to new branch for safe keeping :)

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
 $username = 'username';
 $password = 'password';
 $dbname   = 'dbname';
-
+ 
 $conn = mysqli_connect($hostname, $username, $password) OR die('Unable to connect to database! Please try again later.');
 mysqli_select_db($conn, $dbname); ?>
 </head>
